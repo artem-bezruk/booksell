13ad322e61172c19115833757b66a6e838aedea5
Artem <artem.web.developer.it@gmail.com>
2021-11-01 19:16:45 +0000

    revert root layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {CoreModule} from './core/core.module';
+import {BookModule} from './book/book.module';
+import {APP_BASE_HREF} from '@angular/common';
+import {SharedModule} from './shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    BookModule
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue : '/' }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
