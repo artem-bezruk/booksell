1c1f4ece6c6817688afa4635e9a001e2a52604fa
Artem <artem.web.developer.it@gmail.com>
2021-12-28 04:50:46 +0000

    #2 fix/clean

--- src/app/book/services/book.service.ts
+++ src/app/book/services/book.service.ts
@@ -2,12 +2,11 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {BookSearch} from '../models/book-search';
-import {AuthService} from '../../auth/services/auth.service';
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
-  constructor(private http: HttpClient, private authService: AuthService) {
+  constructor(private http: HttpClient) {
   }
   searchBooks(isbn): Observable<BookSearch> {
     const params = new HttpParams().append('isbn', isbn);
