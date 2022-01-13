d04a2ced719d7dbfde6f5fe104575eff7473c2b5
Artem <artem.web.developer.it@gmail.com>
2022-01-13 19:11:36 +0000

    #2 display result only if result

--- src/app/book/components/book-add/book-add.component.ts
+++ src/app/book/components/book-add/book-add.component.ts
@@ -1,12 +1,14 @@
 import {Component, OnInit} from '@angular/core';
+import {BookService} from '../../services/book.service';
 @Component({
   selector: 'app-book-add',
   templateUrl: './book-add.component.html',
   styleUrls: ['./book-add.component.css']
 })
 export class BookAddComponent implements OnInit {
-  constructor() {
-  }
+  hasResult: boolean;
+  constructor(private bookService: BookService) { }
   ngOnInit() {
+    this.bookService.searchResult.subscribe(next => this.hasResult = next !== null);
   }
 }
