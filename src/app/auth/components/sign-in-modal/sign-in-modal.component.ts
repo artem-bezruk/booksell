8984727fa4c5aad3a2c5c0c5ea78f69bdaae9cf4
Artem <artem.web.developer.it@gmail.com>
2022-05-10 13:17:03 +0000

    #25 update errors display login

--- src/app/auth/components/sign-in-modal/sign-in-modal.component.ts
+++ src/app/auth/components/sign-in-modal/sign-in-modal.component.ts
@@ -2,6 +2,7 @@ import {Component, HostListener, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AuthService} from '../../services/auth.service';
 import {MatDialogRef, MatSnackBar} from '@angular/material';
+import {TranslateService} from '@ngx-translate/core';
 @Component({
   selector: 'app-sign-in-modal',
   templateUrl: './sign-in-modal.component.html',
@@ -12,7 +13,8 @@ export class SignInModalComponent implements OnInit {
   constructor(public snackBar: MatSnackBar,
               private fb: FormBuilder,
               private authService: AuthService,
-              public dialogRef: MatDialogRef<SignInModalComponent, boolean>) {
+              public dialogRef: MatDialogRef<SignInModalComponent, boolean>,
+              private translateService: TranslateService) {
   }
   ngOnInit(): void {
     this.form = this.fb.group({
@@ -30,7 +32,15 @@ export class SignInModalComponent implements OnInit {
     this.authService.obtainAccessToken(email, password).subscribe(
       () => {
         this.dialogRef.close(true);
-      }, error => this.error = error.message);
+      }, err => {
+        let message;
+        if (err.status === 400) {
+          message = this.translateService.instant('SIGN_IN.ERRORS.BAD_REQUEST'  );
+        } else {
+          message = this.translateService.instant('ERRORS.GENERIC');
+        }
+        this.openSnackbar(message);
+      });
   }
   cancel() {
     this.dialogRef.close(false);
@@ -40,5 +50,8 @@ export class SignInModalComponent implements OnInit {
   }
   hasRequiredError(control: string) {
     return this.form.controls[control].hasError('required');
+  }
+  private openSnackbar(message: string) {
+    this.snackBar.open(message, this.translateService.instant('SNACKBAR.ACTION.CLOSE'));
   }
 }
