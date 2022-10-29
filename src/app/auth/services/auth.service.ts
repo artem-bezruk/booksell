24d1a7d85ffa738ca369f35ab4de7ecd4ea48d7b
Artem <artem.web.developer.it@gmail.com>
2022-10-29 15:56:20 +0000

    #68 start move to strict

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -5,47 +5,49 @@ import {CookieService} from 'ngx-cookie-service';
 import {BehaviorSubject, Observable} from 'rxjs';
 import {shareReplay, tap} from 'rxjs/operators';
 import {ConfigService} from '../../core/services/config.service';
-import {OAuthConfig} from '../../core/model/appConfig';
-import {SubjectSubscriber} from 'rxjs/internal/Subject';
+import {AppConfig, OAuthConfig} from '../../core/model/appConfig';
 import {User} from '../../shared/models/user';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private static USER_LOCAL_STORAGE_NAME = 'current_user';
-  private oauthConfig: OAuthConfig;
+  private oauthConfig: OAuthConfig | null = null;
   constructor(private router: Router,
               private http: HttpClient,
               private cookieService: CookieService,
               private configService: ConfigService) {
-    this.configService.appConfig.subscribe(appConfig => this.oauthConfig = appConfig ? appConfig.oauth : null);
-    this._currentUser = new BehaviorSubject<User>(JSON.parse(localStorage.getItem(AuthService.USER_LOCAL_STORAGE_NAME)));
+    this.configService.appConfig.subscribe((appConfig: AppConfig) => this.oauthConfig = appConfig ? appConfig.oauth : null);
+    this._currentUser = new BehaviorSubject<User | null>(JSON.parse(localStorage.getItem(AuthService.USER_LOCAL_STORAGE_NAME) || ''));
   }
-  private _currentUser = new BehaviorSubject(null);
-  get currentUser(): Observable<boolean> {
+  private _currentUser: BehaviorSubject<User | null>;
+  get currentUser(): Observable<User | null> {
     return this._currentUser.asObservable();
   }
-  login(username, password): Observable<any> {
-    const oauthAuth = `${this.oauthConfig.client_id}:${this.oauthConfig.client_secret}`;
-    const params = new HttpParams()
-      .set('username', username)
-      .set('password', password)
-      .set('grant_type', 'password');
-    const headers = new HttpHeaders()
-      .set('Content-type', 'application/x-www-form-urlencoded')
-      .set('Authorization', `Basic ${window.btoa(oauthAuth)}`);
-    const observable = this.http.post('/auth/oauth/token', params, {headers}).pipe(shareReplay());
-    observable.subscribe(
-      (data: any) => this.saveToken({email: username, tokens: { access: data.access_token, refresh: data.refresh_token}}),
-      err => console.error('Invalid Credentials: ', err));
-    return observable;
+  login(username: string, password: string): Observable<any> {
+    if (this.oauthConfig !== null) {
+      const oauthAuth = `${this.oauthConfig.client_id}:${this.oauthConfig.client_secret}`;
+      const params = new HttpParams()
+        .set('username', username)
+        .set('password', password)
+        .set('grant_type', 'password');
+      const headers = new HttpHeaders()
+        .set('Content-type', 'application/x-www-form-urlencoded')
+        .set('Authorization', `Basic ${window.btoa(oauthAuth)}`);
+      const observable = this.http.post('/auth/oauth/token', params, {headers}).pipe(shareReplay());
+      observable.subscribe(
+        (data: any) => this.saveToken({email: username, tokens: {access: data.access_token, refresh: data.refresh_token}}),
+        err => console.error('Invalid Credentials: ', err));
+      return observable;
+    }
+    return new Observable<any>();
   }
   saveToken(user: User) {
     localStorage.setItem(AuthService.USER_LOCAL_STORAGE_NAME, JSON.stringify(user));
     this._currentUser.next(user);
     this.router.navigate(['/']);
   }
-  getCurrentUser(): User {
+  getCurrentUser(): User | null {
     return this._currentUser.value;
   }
   logout() {
@@ -54,11 +56,13 @@ export class AuthService {
     this.router.navigate(['/']);
   }
   refreshToken() {
-    const email = this._currentUser.value.email;
-    return this.http.post('/auth/oauth/token/refresh', {
-      refreshToken: this._currentUser.value.refreshToken
-    }).pipe(tap((tokens: any) => {
-      this._currentUser.next({email, tokens: { access: tokens.access_token, refresh: tokens.refresh_token}});
-    }));
+    if (this._currentUser.value !== null && this._currentUser.value.tokens) {
+      const email = this._currentUser.value.email;
+      return this.http.post('/auth/oauth/token/refresh', {
+        refreshToken: this._currentUser.value.tokens.refresh
+      }).pipe(tap((tokens: any) => {
+        this._currentUser.next({email, tokens: {access: tokens.access_token, refresh: tokens.refresh_token}});
+      }));
+    }
   }
 }
