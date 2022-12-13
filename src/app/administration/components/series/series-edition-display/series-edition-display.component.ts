0b425c49cbd3da0c374d3f685859378ef82203f3
Artem <artem.web.developer.it@gmail.com>
2022-12-13 09:58:15 +0000

    update form and design

--- src/app/administration/components/series/series-edition-display/series-edition-display.component.ts
+++ src/app/administration/components/series/series-edition-display/series-edition-display.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Series} from '../../../../core/model/series';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 @Component({
@@ -8,8 +8,7 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class SeriesEditionDisplayComponent implements OnInit {
   @Input()
   series: Series = {seriesBookCount: 0};
-  @Output()
-  updateSeries: EventEmitter<Series> = new EventEmitter<Series>();
+  @Input()
   form: FormGroup = this.fb.group({
     name: this.fb.control(''),
     seriesBookCount: this.fb.control('', Validators.min(1)),
@@ -18,11 +17,6 @@ export class SeriesEditionDisplayComponent implements OnInit {
   }
   ngOnInit() {
     this.initForm();
-  }
-  submit(): void {
-    const series = this.form.value;
-    series.editor = this.series.editor;
-    this.updateSeries.emit(series);
   }
   initForm(): void {
     this.form.setValue({
--- src/app/administration/components/series/series-list/series-list.component.ts
+++ src/app/administration/components/series/series-list/series-list.component.ts
@@ -2,17 +2,31 @@ import {Component, OnInit} from '@angular/core';
 import {SeriesAdministrationService} from '../../../services/series-administration.service';
 import {Observable} from 'rxjs';
 import {Series} from '../../../../core/model/series';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-series-list',
   templateUrl: './series-list.component.html'
 })
 export class SeriesListComponent implements OnInit {
   seriesList: Observable<Series[]> = this.seriesService.seriesListFiltered;
-  constructor(private seriesService: SeriesAdministrationService) {
+  form = this.fb.group({
+    series: this.fb.array([])
+  });
+  constructor(private seriesService: SeriesAdministrationService, private fb: FormBuilder) {
   }
   ngOnInit() {
+    this.seriesList.subscribe(seriesList => {
+      this.getSeriesArray().clear();
+      seriesList.forEach(() => this.getSeriesArray().push(this.fb.group({
+        name: this.fb.control(''),
+        seriesBookCount: this.fb.control('', Validators.min(1)),
+      })));
+    });
   }
-  onSeriesUpdate($event: Series) {
-    this.seriesService.updateSeries($event);
+  getSeriesArray(): FormArray {
+    return (this.form.get('series') as FormArray);
+  }
+  submit() {
+    this.seriesService.updateSeries({seriesBookCount: 0});
   }
 }
