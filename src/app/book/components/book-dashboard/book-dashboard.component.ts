150808cdc76f1a6c0710daf9aa106acd1e39d34b
Artem <artem.web.developer.it@gmail.com>
2023-03-23 15:36:09 +0000

    BOOK-19 Add title and add book button

--- src/app/book/components/book-dashboard/book-dashboard.component.ts
+++ src/app/book/components/book-dashboard/book-dashboard.component.ts
@@ -4,6 +4,7 @@ import {Observable} from 'rxjs';
 import {CoreService} from '../../../core/services/core.service';
 import {Statistics} from '../../../core/model/statistics';
 import {SimpleChartData} from '../../../core/model/simpleChartData';
+import {AuthService} from '../../../auth/services/auth.service';
 @Component({
   selector: 'app-book-dashboard',
   templateUrl: './book-dashboard.component.html',
@@ -13,13 +14,16 @@ export class BookDashboardComponent implements OnInit {
   public statistics: Observable<Statistics> = this.statisticService.searchResult;
   isLoading: Observable<boolean> = this.coreService.isLoading;
   constructor(private statisticService: StatisticService,
-              private coreService: CoreService
-  ) {
+              private coreService: CoreService,
+              private authService: AuthService) {
   }
   ngOnInit() {
     this.statisticService.getAllStats();
   }
   statusLabelFormat(c: any): string {
     return `${c.label}<br/><small class="number-card-label">${c.data.extra.editor}</small>`;
+  }
+  isAuthenticated() {
+    return this.authService.isAuthenticated();
   }
 }
