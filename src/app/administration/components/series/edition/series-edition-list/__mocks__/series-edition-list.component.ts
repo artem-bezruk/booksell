191bfb0830da5ce557505ef5ccb42c8fbf29efc7
Artem <artem.web.developer.it@gmail.com>
2023-06-24 21:12:53 +0000

    Update tests

---
+++ src/app/administration/components/series/edition/series-edition-list/__mocks__/series-edition-list.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+import {SeriesAdministrationService} from '../../../../services/series-administration.service';
+import {Observable} from 'rxjs';
+import {Series} from '../../../../../core/model/series';
+
+@Component({
+  selector: 'app-series-edition-list',
+  templateUrl: './series-edition-list.component.html',
+  styleUrls: ['../../../../administration-edition.scss']
+})
+export class SeriesEditionListComponent implements OnInit {
+
+  seriesList: Observable<Series[]> = this.seriesService.listFiltered;
+
+  constructor(private seriesService: SeriesAdministrationService) {
+  }
+
+  ngOnInit() {
+  }
+}
