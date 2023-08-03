0f3cc772317da3f8dd60871a63cbbeb5aaaab18c
Artem <artem.web.developer.it@gmail.com>
2023-08-03 20:03:18 +0000

    Fix linter

--- src/app/administration/components/bookType/gestion/book-type-gestion-list/book-type-gestion-list.component.spec.ts
+++ src/app/administration/components/bookType/gestion/book-type-gestion-list/book-type-gestion-list.component.spec.ts
@@ -1,7 +1,11 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {BookTypeGestionListComponent} from './book-type-gestion-list.component';
-import {MockBookTypeEditionListDisplayComponent} from '../book-type-edition-list-display/__mocks__/book-type-edition-list-display.component';
-import {MockBookTypeCreationListDisplayComponent} from '../book-type-creation-list-display/__mocks__/book-type-creation-list-display.component';
+import {
+  MockBookTypeEditionListDisplayComponent
+} from '../book-type-edition-list-display/__mocks__/book-type-edition-list-display.component';
+import {
+  MockBookTypeCreationListDisplayComponent
+} from '../book-type-creation-list-display/__mocks__/book-type-creation-list-display.component';
 import {MatListModule} from '@angular/material/list';
 import {BookTypeAdministrationService} from '../../../../services/book-type-administration.service';
 import {bookTypeAdministrationServiceMock} from '../../../../services/__mocks__/book-type-administration.service';
